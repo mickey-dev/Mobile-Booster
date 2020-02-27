c24ab297cdd034d653a82ba93cfafed0df35f9d4
BlueSky <alister11david@gmail.com>
2020-02-27 22:34:53 +0300

    Updated shop page

--- themes/booster-template/includes/woocommerce/actions.php
+++ themes/booster-template/includes/woocommerce/actions.php
@@ -1,4 +1,17 @@
 <?php
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+add_action( 'woocommerce_before_shop_loop_item', 'woocommerce_show_product_loop_sale_flash', 10 );
+
+add_action('woocommerce_shop_loop_item_title','woocommerce_template_loop_product_link_open',5);
+add_action('woocommerce_shop_loop_item_title','woocommerce_template_loop_product_link_close',15);
+
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
+add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_link_open', 5 );
+add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_link_close', 15 );
+
+remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
+
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
 
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_title',5);
 add_action('woocommerce_before_single_product_summary','woocommerce_template_single_title',5);
@@ -6,9 +19,6 @@ add_action('woocommerce_before_single_product_summary','woocommerce_template_sin
 
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_price',10);
 //add_action('woocommerce_before_add_to_cart_button','woocommerce_template_single_price',60);
-
-
-
 
 remove_action('woocommerce_before_shop_loop','woocommerce_result_count',20);
 remove_action('woocommerce_before_shop_loop','woocommerce_catalog_ordering',30);
