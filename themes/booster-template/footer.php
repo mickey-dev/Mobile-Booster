dc4706b7c9d2c52ad2ed52683a5a05f472917ca5
Mickey <you@example.com>
2020-02-28 11:34:52 +0300

    home

--- themes/booster-template/footer.php
+++ themes/booster-template/footer.php
@@ -23,28 +23,28 @@ $ins= theme_option( 'ins' );
 ?>
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="col-xs-12 col-sm-12 guarantee">
-        <div class="container header-bottom">
-            <div class="row">
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 header-bottom-icons">
+    <div class="row guarantee">
+        <div class="header-bottom">
+            <div class="container">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 header-bottom-icons">
                     <img class="combine1 sprite-guarantee" src="<?php echo get_template_directory_uri() ?>/assets/img/1.png" alt="Mobile Booster Guarantee">
                     <div class="header-icons-span">
                         <span>Free Delivery</span> <br> <span>in the UK</span>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 header-bottom-icons">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 header-bottom-icons">
                     <img class="combine1 sprite-guarantee" src="<?php echo get_template_directory_uri() ?>/assets/img/2.png" alt="Mobile Booster Guarantee">
                     <div class="header-icons-span">
                         <span>30 Day Money-Back</span> <br> <span>Guarantee</span>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 header-bottom-icons">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 header-bottom-icons">
                     <img class="combine1 sprite-guarantee" src="<?php echo get_template_directory_uri() ?>/assets/img/3.png" alt="Mobile Booster Guarantee">
                     <div class="header-icons-span">
                         <span>Five Bar Signal</span> <br> <span>Guarantee</span>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 header-bottom-icons">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 header-bottom-icons">
                     <img class="combine1 sprite-guarantee" src="<?php echo get_template_directory_uri() ?>/assets/img/4.png" alt="Mobile Booster Guarantee">
                     <div class="header-icons-span">
                         <span>Works On Every </span> <br> <span>Network & Carrier</span>
