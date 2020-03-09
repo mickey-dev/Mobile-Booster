9a90fe75985ce6a98490d87adbe55e89ad884861
BlueSky <alister11david@gmail.com>
2020-03-09 15:56:58 +0300

    Updated filter

--- themes/booster-template/page-filter.php
+++ themes/booster-template/page-filter.php
@@ -1,6 +1,15 @@
 <?php
 $post = $_POST;
 
+function provider_in_data($providers) {
+    $provider_post = $_POST['data']['provider'];
+    foreach ($providers as $prov_item) {
+        if (in_array($prov_item, $provider_post)) {
+            return true;
+        }
+    }
+    return false;
+}
 if ( !isset($post['data']) || empty($post['data'])  )
     wp_redirect(home_url());
 
@@ -39,21 +48,25 @@ $args = array(
 
 $products = new WP_Query( $args );
 
+$providers_900 = [203, 202];
+$providers_1800 = [200, 130, 201, 189];
+$providers_2100 = [198, 197];
 
-if($products->have_posts()) {
-    while ($products->have_posts()) { $products->the_post();
+if (!$products->have_posts() || (!provider_in_data($providers_900) && provider_in_data($providers_1800) && provider_in_data($providers_2100))) {
+    get_header();
+    ?>
+    <div class="container" style="min-height: 500px">
+        <h1 class="page-title"><?php the_title(); ?></h1>
+        <?php wc_get_template('loop/no-products-found.php'); ?>
+    </div>
+    <?php
+    get_footer();
+} else {
+    while ($products->have_posts()) {
+        $products->the_post();
         wp_redirect(get_permalink());
         exit();
         wc_get_template_part('content', 'product_list');
     }
-} else{
-    get_header();
-?>
-    <div class="container">
-        <h1 class="page-title"><?php the_title(); ?></h1>
-        <?php wc_get_template( 'loop/no-products-found.php' ); ?>
-    </div>
-<?php
-    get_footer();
 }
 ?>
